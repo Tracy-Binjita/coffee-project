e37068294add965d86348484d661995aeee27d70
Tracy Velasquez <velasqueztracy25@gmail.com>
2020-07-13 13:37:33 -0500

    reorrganzied conditionals

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var searchBar = document.getElementById("coffee-search")
 searchBar.addEventListener("input", updateCoffees)
-document.getElementById("roast-selection").addEventListener("change",updateCoffees)
+document.getElementById("roast-selection").addEventListener("change", updateCoffees)
 
 function renderCoffee(coffee) {
     var html = '<ul class="coffee" id="coffee"><div>';
@@ -21,44 +21,45 @@ function renderCoffees(coffees) {
     return html;
 }
 
+//Filters by roast selection then searches by input. Calls function every time a new letter is typed using eventListeners.
+
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
     var filteredCoffees = [];
     console.log("test")
     coffees.forEach(function (coffee) {
-    if (coffee.name.toLowerCase().includes(searchBar.value.toLowerCase())){
         if (coffee.roast === selectedRoast || selectedRoast === "") {
-            filteredCoffees.push(coffee);
+            if (coffee.name.toLowerCase().includes(searchBar.value.toLowerCase())) {
+                filteredCoffees.push(coffee);
+            }
         }
-    }
     });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
+    coffeeList.innerHTML = renderCoffees(filteredCoffees);
 }
 
-
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
-var coffees = [
-    {id: 1, name: 'Light City', roast: 'light'},
-    {id: 2, name: 'Half City', roast: 'light'},
-    {id: 3, name: 'Cinnamon', roast: 'light'},
-    {id: 4, name: 'City', roast: 'medium'},
-    {id: 5, name: 'American', roast: 'medium'},
-    {id: 6, name: 'Breakfast', roast: 'medium'},
-    {id: 7, name: 'High', roast: 'dark'},
-    {id: 8, name: 'Continental', roast: 'dark'},
-    {id: 9, name: 'New Orleans', roast: 'dark'},
-    {id: 10, name: 'European', roast: 'dark'},
-    {id: 11, name: 'Espresso', roast: 'dark'},
-    {id: 12, name: 'Viennese', roast: 'dark'},
-    {id: 13, name: 'Italian', roast: 'dark'},
-    {id: 14, name: 'French', roast: 'dark'},
-];
+    var coffees = [
+        {id: 1, name: 'Light City', roast: 'light'},
+        {id: 2, name: 'Half City', roast: 'light'},
+        {id: 3, name: 'Cinnamon', roast: 'light'},
+        {id: 4, name: 'City', roast: 'medium'},
+        {id: 5, name: 'American', roast: 'medium'},
+        {id: 6, name: 'Breakfast', roast: 'medium'},
+        {id: 7, name: 'High', roast: 'dark'},
+        {id: 8, name: 'Continental', roast: 'dark'},
+        {id: 9, name: 'New Orleans', roast: 'dark'},
+        {id: 10, name: 'European', roast: 'dark'},
+        {id: 11, name: 'Espresso', roast: 'dark'},
+        {id: 12, name: 'Viennese', roast: 'dark'},
+        {id: 13, name: 'Italian', roast: 'dark'},
+        {id: 14, name: 'French', roast: 'dark'},
+    ];
 
-var tbody = document.querySelector('#coffees');
-var submitButton = document.querySelector('#submit');
-var roastSelection = document.querySelector('#roast-selection');
+    var coffeeList = document.querySelector('#coffees');
+    var submitButton = document.querySelector('#submit');
+    var roastSelection = document.querySelector('#roast-selection');
 
-tbody.innerHTML = renderCoffees(coffees);
+    coffeeList.innerHTML = renderCoffees(coffees);
 
-submitButton.addEventListener('click', updateCoffees);
+    submitButton.addEventListener('click', updateCoffees);
