d36273e2737a36422a47eb1102cd102ea917d6fb
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-07-14 10:52:33 -0500

    exercise

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function renderCoffee(coffee) { 
     html += '<h5>' + coffee.name + '</h5>'; 
     html += '<h6 class="float-right align-self-center pl-2">' + coffee.roast + '</h6>'; 
     html += '</div>'; 
-    return html; 
+    return html;
 }
 
 function renderCoffees(coffees) {
